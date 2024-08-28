914986330d8e3d8ed181b714577ef899db038917
andre-cat <andrecat-@outlook.com>
2024-08-28 00:56:48 -0500

    Name reset

--- js/encoding.js
+++ js/encoding.js
@@ -2,53 +2,55 @@ const removeAccents = str => str.normalize('NFD').replace(/[\u0300-\u036f]/g, ''
 
 function encode() {
     enableNoOutputImage();
-
+    enableCopyButton();
+    
     let inputField = document.getElementById("input-field");
-
+    
     let outputField = document.getElementById("output-field");
 
     let inputText = inputField.value;
     inputText = removeAccents(inputText);
-
+    
     let outputText = "";
-
+    
     for (const char of inputText) {
         switch (char) {
             case "a":
                 outputText = outputText + "ai";
                 break;
-            case "e":
-                outputText = outputText + "enter";
-                break;
-            case "i":
-                outputText = outputText + "imes";
+                case "e":
+                    outputText = outputText + "enter";
+                    break;
+                    case "i":
+                        outputText = outputText + "imes";
                 break;
             case "o":
                 outputText = outputText + "ober";
                 break;
-            case "u":
-                outputText = outputText + "ufat";
+                case "u":
+                    outputText = outputText + "ufat";
                 break;
             default:
                 outputText = outputText + char;
                 break;
+            }
         }
-    }
 
-    inputField.value = inputText;
-    outputField.innerText = outputText;
-}
+        inputField.value = inputText;
+        outputField.innerText = outputText;
+    }
 
 function decode() {
     enableNoOutputImage();
+    enableCopyButton();
     
     let inputField = document.getElementById("input-field");
-
+    
     let outputField = document.getElementById("output-field");
-
+    
     let inputText = inputField.value;
     inputText = removeAccents(inputText);
-
+    
     let outputText = inputText;
 
     outputText = outputText.replace("ai", "a");
@@ -56,7 +58,7 @@ function decode() {
     outputText = outputText.replace("imes", "i");
     outputText = outputText.replace("ober", "o");
     outputText = outputText.replace("ufat", "u");
-
+    
     inputField.value = inputText;
     outputField.innerText = outputText;
 }
